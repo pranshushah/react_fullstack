c833beffaa8865821c43ad8c30b25fbf732c5385
pranshushah <pranshu.shah23@gmail.com>
2018-09-30 13:03:52 +0530

    solved some error

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,18 @@ import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './store/reducers/index';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
-const composeEnhancers =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
-const store = createStore(
-  reducers,
-  {},
-  composeEnhancers(applyMiddleware(reduxThunk))
-);
+// const composeEnhancers =
+//   process.env.NODE_ENV === 'development'
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     : null || compose;
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 const app = (
   <Provider store={store}>
